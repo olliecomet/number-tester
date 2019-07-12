736c5c5980fd2f9348913f1c7ecba934deb79296
ollie <soulshaymin16@gmail.com>
2019-07-11 20:40:02 -0700

    passed both tests

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,33 @@
+import isEven from '../is-even.js';
+
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
+test('is even', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const even = 2;
+    const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
+    const yourNumber = isEven(even);
 
     //Assert
     // What should the result be?
-    assert.equal(true, false);
+    assert.equal(yourNumber, expected);
+});
+
+test('is odd', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const odd = 3;
+    const expected = false;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const yourNumber = isEven(odd);
+
+    //Assert
+    // What should the result be?
+    assert.equal(yourNumber, expected);
 });
